22549e6b3e79dd5a8bd902d68152fcbc2dbb2bee
Anshika Dubey <anshikadubey144@gmail.com>
2024-12-20 17:57:41 +0530

    Some updation

--- JenkinsPracticeProject/src/main/java/com/comcast/crm/generic/fileutility/IPathConstant.java
+++ JenkinsPracticeProject/src/main/java/com/comcast/crm/generic/fileutility/IPathConstant.java
@@ -2,12 +2,12 @@ package com.comcast.crm.generic.fileutility;
 
 public interface IPathConstant {
 	
-	String filepath="./configAppData\\CommonData.properties";
-	String excelpath="./testData\\CeateOrg.xlsx";
+	String filepath="./ConfigData\\CommonData.properties";
+	String excelpath="./TestScript\\CeateOrg.xlsx";
 	String DB_URL="jdbc:mysql://49.249.28.218:3333/ninza_hrm";  //ninza hrm
 	String DB_UN="root";
 	String DB_PWD="root";
-	String JSON_path="configAppData\\CommonData.json";
+	String JSON_path="./ConfigData\\CommonData.json";
 	String db_url="jdbc:mysql://localhost:3306/tekpyramid";
 
 }
